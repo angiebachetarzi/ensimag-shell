abe3a0b78f7fa0276117221e8a3d72575891e0bb
angiebachetarzi <bachetarzi.angie@hotmail.com>
2020-02-02 10:57:10 +0100

    jobs done

--- src/ensishell.c
+++ src/ensishell.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "variante.h"
 #include "readcmd.h"
@@ -57,6 +58,41 @@ void terminate(char *line) {
 	  free(line);
 	printf("exit\n");
 	exit(0);
+}
+
+//structure of list of processes in background
+struct proc_jobs {
+	pid_t pid;	//pid of the command
+	char *cmd; //command to be stored
+	int index; //index of process in list of jobs
+	struct proc_jobs *next;
+};
+
+struct proc_jobs* list_bg_processes;
+
+int add_process_to_jobs(pid_t pid_bg, char *cmd_bg) {
+	struct proc_jobs *bg_element = malloc(sizeof (struct proc_jobs));
+
+	bg_element -> pid = pid_bg;
+	bg_element -> cmd = malloc((sizeof(cmd_bg) + 1) * sizeof(char));
+	strcpy(bg_element -> cmd, cmd_bg);
+	bg_element -> next = NULL;
+
+	//if list is empty
+	if (list_bg_processes == NULL) {
+		bg_element -> index = 1;
+		list_bg_processes = bg_element;
+	} else {
+		//loop through list to get last element
+		struct proc_jobs *tmp_element = list_bg_processes;
+		while (tmp_element -> next != NULL) {
+			(tmp_element -> index)++;
+			tmp_element = tmp_element -> next;
+		}
+		bg_element -> index = (tmp_element -> index) + 1;
+		tmp_element -> next = bg_element;
+	}
+	return bg_element -> index;
 }
 
 
@@ -115,29 +151,54 @@ int main() {
 			printf("error: %s\n", l->err);
 			continue;
 		}
+		//handle the case where you just press enter (without a command)
+		if (l -> seq[0] == NULL) {
+			continue;
+		}
+
+		//identifying the command jobs in the command line
+		if (strcmp(l -> seq[0][0], "jobs") == 0) {
+			//loop through list to and display processes
+			struct proc_jobs *tmp_element = list_bg_processes;
+			while (tmp_element != NULL) {
+				printf("[%d] %d, cmd : %s\n", tmp_element -> index, tmp_element -> pid, tmp_element -> cmd);
+				tmp_element = tmp_element -> next;
+			}
+			continue;
+		}
 
 		if (l->in) printf("in: %s\n", l->in);
 		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
+		if (l->bg) //printf("background (&)\n");
 
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
-			pid_t pid;
+			pid_t pid1, pid2;;
 			int status;
-			pid = fork();
-			switch (pid) {
+			pid1 = fork();
+			switch (pid1) {
 				case -1 : 
 					perror("error in fork\n");
 					break;
 				case 0 :
+				//case we have one more command
+				//example : |
+				if (l -> seq[1] != NULL) {
+					pid2 = fork();
+					switch (pid2) {
+						
+					}
+				}
 					execvp(cmd[0], cmd);
 					break;
 				default :
 				if (!l -> bg) {
-					waitpid(pid, &status, 0);
+					waitpid(pid1, &status, 0);
 				} else {
 					//create a new process and add it to a list of processes
+					int index = add_process_to_jobs(pid1, *cmd);
+					printf("[%d] %d\n", index, pid1);
 				}
 					break;
 
